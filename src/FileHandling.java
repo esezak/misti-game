81af152f37db112d7831102bccee0be2e3590d67
CastorDev <egesezaki03@gmail.com>
2023-05-10 19:47:42 +0300

    Changed arrays for arrylists in leaderboard

--- src/FileHandling.java
+++ src/FileHandling.java
@@ -8,12 +8,13 @@ public class FileHandling {
     private Scanner reader = null;//both
     private Formatter formatter = null;//leaderboard
     private FileWriter writer = null;//leaderboard
-    private String[] names = new String[10];//leaderboard
-    private String[] scores = new String[10];//leaderboard
+    private ArrayList<String> names = new ArrayList<>();//leaderboard
+    private ArrayList<String> scores = new ArrayList<>();//leaderboard
     private Player player;//leaderboard
     private String point;//leaderboard
     private String fileName;//points
     private ArrayList<Card> deck;//points
+    private int listSize;
     public FileHandling(String pointFileName,ArrayList<Card> deck){
         fileName = pointFileName;
         this.deck=deck;
@@ -28,22 +29,22 @@ public class FileHandling {
                 String[] components;
                 String s;
                 try{
-                if(!line.equals("")) {
-                    components = line.split(" ");
-                    char symbol = components[0].charAt(0);
-                    String number = components[0].substring(1);
-                    int point = Integer.parseInt(components[1]);//can throw error   INFO
-                    s = Character.toString(symbol);
+                    if(!line.equals("")) {
+                        components = line.split(" ");
+                        char symbol = components[0].charAt(0);
+                        String number = components[0].substring(1);
+                        int point = Integer.parseInt(components[1]);
+                        s = Character.toString(symbol);
 
-                    if (s.equals("H") || s.equals("h")) {// converts symbol to unicode counterparts
-                        s = "♥";
-                    } else if (s.equals("S") || s.equals("s")) {
-                        s = "♠";
-                    } else if (s.equals("C") || s.equals("c")) {
-                        s = "♣";
-                    } else if (s.equals("D") || s.equals("d")) {
-                        s = "♦";
-                    }
+                        if (s.equals("H") || s.equals("h")) {// converts symbol to unicode counterparts
+                            s = "♥";
+                        } else if (s.equals("S") || s.equals("s")) {
+                            s = "♠";
+                        } else if (s.equals("C") || s.equals("c")) {
+                            s = "♣";
+                        } else if (s.equals("D") || s.equals("d")) {
+                            s = "♦";
+                        }
 
                     for (Card card : deck) {
                         if (card.getNumber().equals(number) && card.getSymbol().equals(s) && card.getPoint() == 1) {
@@ -59,14 +60,14 @@ public class FileHandling {
                     }
                 }
                 }catch(NumberFormatException numex){
-                    System.out.println("Please use the format: <symbol><number> <point value>");
+                    System.err.println("Please use the format: <symbol><number> <point value>");
                 }
 
             }
         } catch (IOException noFile) {
-            System.out.println("File not found using default values");
+            System.err.println("File not found, using default values...");
         } catch(NumberFormatException numberFormatException){
-            //System.out.println("Please use the format: <symbol><number> <point value>");
+            System.err.println("number format error on reading point");
         }
         finally {
             if (reader != null) {
@@ -84,62 +85,60 @@ public class FileHandling {
         writeToFile();
     }
     private void read() {
+        boolean hasBeenAdded = false;
         try {
             reader = new Scanner(Paths.get("scores.txt"));
-            int counter = 0;
             while (reader.hasNextLine()) {
-                String[] info = reader.nextLine().split(",");
-                scores[counter] = info[1].trim();
-                names[counter] = info[0].trim();
-                counter++;
+                try {
+                    String line = reader.nextLine();
+                    String[] info = line.split(",");
+                    if (player.getPoint() > Integer.parseInt(info[1].trim()) && !hasBeenAdded) {
+                        scores.add(point);
+                        names.add(player.getName());
+                        hasBeenAdded = true;
+                    }
+                    scores.add(info[1].trim());
+                    names.add(info[0].trim());
+                }catch(NumberFormatException numerr){
+                    System.err.println("Incorrect point type! Removing entry...");
+                }catch(ArrayIndexOutOfBoundsException array){
+                    System.out.println("Skipping empty or broken line...");
+                }
             }
         } catch (IOException e) {
             System.out.println("File not found, is this your first time playing?");
-        } catch(ArrayIndexOutOfBoundsException array){
-            System.out.println("Unexpected file format");
         } catch(Exception err){
-            System.out.println("An error has occurred!");
+            System.err.println("A read error has occurred!");
         }
         finally {
             if (reader != null) {
                 reader.close();
             }
         }
-        for (int i = 0; i < names.length; i++) {
-            try {
-                if (scores[i] != null) {
-                    if (Integer.parseInt(scores[i]) < player.getPoint()) {
-                        for (int j = names.length - 1; j > i; j--) {//make place for new value
-                            names[j] = names[j - 1];
-                            scores[j] = scores[j - 1];
-                        }
-                        names[i] = player.getName();// add values
-                        scores[i] = point;
-                        break;
-                    }
-                } else if (scores[i] == null) {// if empty add the value
-                    scores[i] = point;
-                    names[i] = player.getName();
-                    break;
-                }
-            } catch(NumberFormatException number){
-                System.out.println("Incorrect score type, removing entry");
-                names[i] = null;
-                scores[i] = null;
-            }
+        if(!hasBeenAdded){
+            scores.add(point);
+            names.add(player.getName());
+        }
+    }
+    private void setListSize(){//sets the list size to 10 if list size is bigger than 10
+        if(names.size()>10){
+            listSize = 10;
+        } else{
+            listSize = names.size();
         }
     }
     private void writeToFile(){
+        setListSize();
         try {
             writer = new FileWriter("scores.txt", false);
-            for(int i=0; i< 10;i++){
+            for(int i=0; i< listSize;i++){
                 formatter = new Formatter(writer);
-                if(scores[i]!=null) {
-                    formatter.format("%s,%s\n", names[i], scores[i]);
-                }
+                //if(scores.get(i)!=null) {
+                    formatter.format("%s,%s\n", names.get(i), scores.get(i));
+                //}
             }
         } catch (Exception e) {
-            System.out.println("Something went wrong.");
+            System.out.println(e.getMessage());
         }finally {
             if (formatter != null){
                 formatter.close();
@@ -150,10 +149,8 @@ public class FileHandling {
         System.out.println();
         System.out.println("----------Hi Scores----------");
         int place=1;
-        for (int i = 0; i < names.length; i++) {
-            if (names[i] != null) {
-                System.out.println((place++)+"# Name: " + names[i] + "   Score: " + scores[i]);
-            }
+        for (int i = 0; i < listSize; i++) {
+                System.out.println((place++)+"# Name: " + names.get(i) + "   Score: " + scores.get(i));
         }
     }
 }
